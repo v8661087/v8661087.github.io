cf70237dec43e3e4c20789bab5551908309bac5d
v8661087 <v8661087@gmail.com>
2019-05-02 12:13:25 +0800

    190502

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var rename      = require("gulp-rename");
 
 //定义css任务
 gulp.task('css', function(){
   //定义postcss任务流数组
   var processors = [
     autoprefixer({
-      browsers:['last 3 version'],
+      browsers:['last 5 version'],
       cascade: false,
       remove: false
     }),
   ];
-  return gulp.src('./css/*.css')
+  return gulp.src('./src/*.css')
              .pipe(postcss(processors))
+             .pipe(minifyCSS())
+             .pipe(rename(function(path) {
+              path.basename += ".min";
+              path.extname = ".css";
+            }))
              .pipe(gulp.dest('./dist'));
 });
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require("webpack");
+var autoprefixer = require("autoprefixer");
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports={
+    context: path.resolve(__dirname, 'src'),
+    entry:{
+      index:['./index.js','./style.css']
+    },
+    output:{
+        filename:'index.bundle.js',
+        path: path.resolve(__dirname,'./dist')
+    },
+    mode: 'production',
+    plugins:[
+      new MiniCssExtractPlugin({
+        filename: "./style.bundle.css",
+      }),
+      new webpack.LoaderOptionsPlugin({
+        options: {
+            postcss: [
+                autoprefixer()
+            ]
+        }
+      }),
+      new OptimizeCssAssetsPlugin({assetNameRegExp: /\.css$/g,
+        cssProcessorOptions: {
+          safe: true,
+          autoprefixer: { disable: true },
+          mergeLonghand: false,
+          discardComments: {
+            removeAll: true // 移除注释
+          }
+        },
+        canPrint: true
+      })
+    ],
+    module: {
+      rules: [
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "postcss-loader"
+            ]
+        }
+      ]
+    },
+    
+}
