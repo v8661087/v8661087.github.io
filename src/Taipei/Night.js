5cba6b0555854231322959b4741156d4d58b366e
v8661087 <v8661087@gmail.com>
2020-06-30 19:04:57 +0800

    update nightmarket add loading and reduce request

--- src/Taipei/Night.js
+++ src/Taipei/Night.js
@@ -1,52 +1,79 @@
-function GetMarket() {
-  $("#listContent").empty();
-  $("#NightMarket").empty();
-  $.ajax({
+let market = [];
+let food = [];
+async function getMarket() {
+  await $.ajax({
     url:
       "https://data.taipei/api/getDatasetInfo/downloadResource?id=8a569b5b-0cb1-4c7f-a064-780b3301354c&rid=9744ea1c-aafb-471c-8bb8-04f398ad415a",
-    dataType: "json"
-  }).done(function(info) {
-    for (i = 0; i < info.length; i++) {
-      if (info[i].夜市名稱 === $("#mySelect").val()) {
-        if (info[i].交通資訊 !== null) {
-          $("#NightMarket").append(
-            `<div>夜市名稱:${info[i].夜市名稱}<br/>
-              交通資訊${info[i].交通資訊}<br/>
-              所在位置:${info[i].所在位置}<br/>
-              營業時間:${info[i].營業時間}</div>`
-          );
-        } else {
-          $("#NightMarket").append(
-            `<div>夜市名稱:${info[i].夜市名稱}<br/>
-              所在位置:${info[i].所在位置}<br/>
-              營業時間:${info[i].營業時間}</div>`
-          );
-        }
-      }
-    }
+    dataType: "json",
+  }).done(function (info) {
+    market = [...info];
   });
+  if (food.length) {
+    $(".loading").css("display", "none");
+  }
 }
-
-function GetFood() {
-  $.ajax({
+async function getFood() {
+  await $.ajax({
     url:
       "https://data.taipei/api/getDatasetInfo/downloadResource?id=8a569b5b-0cb1-4c7f-a064-780b3301354c&rid=da4a214f-0c3d-437e-9312-34e5671db2a8",
-    dataType: "json"
-  }).done(function(msg) {
-    for (i = 0; i < msg.length; i++) {
-      if (msg[i].夜市名稱 === $("#mySelect").val()) {
-        $("#listContent").append(
-          `<div class='listEach'>
-            <div class='name'>${msg[i].攤位名稱}</div>
-            <div class='food'>${msg[i].其他相關產品}</div>
-          </div>`
-        );
-      }
-    }
+    dataType: "json",
+  }).done(function (info) {
+    food = [...info];
   });
+  if (market.length) {
+    $(".loading").css("display", "none");
+  }
 }
-function GetInformation() {
-  GetMarket();
-  GetFood();
+$(document).ready(function () {
+  $(".loading").css("display", "flex");
+  getMarket();
+  getFood();
+});
+
+function showMarket() {
+  let name = market.find((item) => item.夜市名稱 === $("#mySelect").val());
+  if (name.交通資訊 !== null) {
+    $("#NightMarket").append(
+      `<div>夜市名稱:${name.夜市名稱}<br/>
+              交通資訊${name.交通資訊}<br/>
+              所在位置:${name.所在位置}<br/>
+              營業時間:${name.營業時間}</div>`
+    );
+  } else {
+    $("#NightMarket").append(
+      `<div>夜市名稱:${name.夜市名稱}<br/>
+              所在位置:${name.所在位置}<br/>
+              營業時間:${name.營業時間}</div>`
+    );
+  }
 }
-$("#mySelect").on("change", GetInformation);
+
+function showFood() {
+  let filtered = food.filter((item) => item.夜市名稱 === $("#mySelect").val());
+  for (i = 0; i < filtered.length; i++) {
+    if (filtered[i].夜市名稱 === $("#mySelect").val()) {
+      $("#listContent").append(
+        `<div class='listEach'>
+            <div class='name'>${filtered[i].攤位名稱}</div>
+            <div class='food'>${filtered[i].其他相關產品}</div>
+          </div>`
+      );
+    }
+  }
+}
+
+function showInformation() {
+  $("#listContent").empty();
+  $("#NightMarket").empty();
+  if ($("#mySelect").val() !== "請選擇") {
+    var retry = setInterval(function () {
+      if (market.length && food.length) {
+        clearInterval(retry);
+        showMarket();
+        showFood();
+      }
+    }, 1000);
+  }
+}
+
+$("#mySelect").on("change", showInformation);
