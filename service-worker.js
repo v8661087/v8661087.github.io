76dc51833e94a17a27d58e051518ccdf5a1a30cf
v8661087 <v8661087@gmail.com>
2020-09-30 15:49:52 +0800

    add og:type

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = `static-cache-v1`;
+const CACHE_NAME = `static-cache-v1.01`;
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
   "/",
